c166a17f9d01969226ea03ac9d622bc2ced98838
MounikaSK <142923574+MounikaSK@users.noreply.github.com>
2024-10-06 11:56:20 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,7 @@
+// No import statements are needed for this program
+public class Sample {
+    public static void main(String[] args) {
+        // Simple output message
+        System.out.println("Hello, World!");
+    }
+}
